b6d116b8ede1bcae025489dc4076674de475c337
skliff13 <skliff13@tut.by>
2019-02-07 18:31:05 +0300

    Introducing modified data generator

--- d_Network_Training/train_model.py
+++ d_Network_Training/train_model.py
@@ -10,8 +10,9 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg19 import VGG19
 from keras_applications.resnet50 import ResNet50
-from keras.preprocessing.image import ImageDataGenerator
+# from keras.preprocessing.image import ImageDataGenerator
 from inception_v1 import InceptionV1
+from data_gen import ModifiedDataGenerator
 
 
 def load_data(data_dir, data_shape):
@@ -44,43 +45,50 @@ def load_data(data_dir, data_shape):
     return (x_train_val[0], y_train_val[0]), (x_train_val[1], y_train_val[1])
 
 
-def train_model(batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer):
+def train_model(batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer, crop_to):
     data_shape = (image_sz, image_sz)
     (x_train, y_train), (x_val, y_val) = load_data(data_dir, data_shape)
+
+    if crop_to > 0:
+        final_image_sz = crop_to
+    else:
+        final_image_sz = image_sz
 
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_val = keras.utils.to_categorical(y_val, num_classes)
 
-    model = model_type(weights=None, include_top=True, input_shape=(image_sz, image_sz, 1), classes=num_classes)
+    model = model_type(weights=None, include_top=True, input_shape=(final_image_sz, final_image_sz, 1), classes=num_classes)
 
     pattern = 'model_Sz%i_%s_%s_Ep%i_Lr%.1e*.hdf5'
     pattern = pattern % (image_sz, model_type.__name__, optimizer.__name__, epochs, learning_rate)
 
-    files = glob(pattern)
-    if not files:
-        print('\n### Running training for ' + pattern + '\n')
+    print('\n### Running training for ' + pattern + '\n')
 
-        opt = optimizer(lr=learning_rate, decay=0.0e-6)
-        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    opt = optimizer(lr=learning_rate, decay=0.0e-6)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-        train_gen = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, rescale=1.,
-                                       zoom_range=0.2, fill_mode='nearest', cval=0)
+    train_gen = ModifiedDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, rescale=1.,
+                                   zoom_range=0.2, fill_mode='nearest', cval=0, crop_to=crop_to)
 
-        val_gen = ImageDataGenerator(rescale=1.)
+    val_gen = ModifiedDataGenerator(rescale=1., crop_to=crop_to)
 
-        model.fit_generator(train_gen.flow(x_train, y_train, batch_size),
-                            steps_per_epoch=(x_train.shape[0] + batch_size - 1) // batch_size,
-                            epochs=epochs,
-                            validation_data=val_gen.flow(x_val, y_val),
-                            validation_steps=(x_val.shape[0] + batch_size - 1) // batch_size)
+    model.fit_generator(train_gen.flow(x_train, y_train, batch_size),
+                        steps_per_epoch=(x_train.shape[0] + batch_size - 1) // batch_size,
+                        epochs=epochs,
+                        validation_data=val_gen.flow(x_val, y_val),
+                        validation_steps=(x_val.shape[0] + batch_size - 1) // batch_size)
 
-        predictions = model.predict(x_val, batch_size=batch_size)
-        fpr, tpr, _ = roc_curve(y_val[:, 1].ravel(), predictions[:, 1].ravel())
-        roc_auc = auc(fpr, tpr)
+    if crop_to > 0:
+        x_val = val_gen.crop_data_bacth(x_val)
 
-        model_filename = pattern.replace('*', '_Auc%.3f' % roc_auc)
+    print(x_val.shape)
+    predictions = model.predict(x_val, batch_size=batch_size)
+    fpr, tpr, _ = roc_curve(y_val[:, 1].ravel(), predictions[:, 1].ravel())
+    roc_auc = auc(fpr, tpr)
 
-        model.save_weights(model_filename)
+    model_filename = pattern.replace('*', '_Auc%.3f' % roc_auc)
+    print('Saving model to ' + model_filename)
+    model.save_weights(model_filename)
 
 
 def parse_args():
@@ -93,6 +101,7 @@ def parse_args():
     model_type = argv[6]
     num_classes = int(argv[7])
     optimizer = argv[8]
+    crop_to = int(argv[9])
 
     if model_type == 'VGG16':
         model_type = VGG16
@@ -114,14 +123,16 @@ def parse_args():
     elif optimizer == 'Adam':
         optimizer = keras.optimizers.adam
 
-    return batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer
+    return batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer, crop_to
 
 
 def main():
-    batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer = parse_args()
+    batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer, crop_to = parse_args()
 
-    train_model(batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer)
+    train_model(batch_size, data_dir, epochs, image_sz, learning_rate, model_type, num_classes, optimizer, crop_to)
 
 
 if __name__ == '__main__':
+    # os.sys.argv = 'train_model.py 32 /home/skliff13/work/PTD_Xray/datasets/tuberculosis/v2.2 400 256 0.0001 InceptionV1 2 SGD 224'.split(' ')
+
     main()
